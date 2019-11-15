8e35bf356b7abfff77afef75731f6e6ce0caf190
田村秀子 <hideko@tamurahidekonoMacBook-Air.local>
2019-11-15 20:18:35 +0900

    restore file time

--- sitemap.sh
+++ sitemap.sh
@@ -1,40 +1,17 @@
 #!/bin/bash
-: ${cd:=.} ${URL:="https://ten.j-j.net/"}
-
-IFS=$'\r\n'
-GLOBIGNORE='*'
-OPTIONS=(
--prune
-! -iname CNAME
-! -iname robots.txt
-! -iname sitemap.\*
-! -iname \*.yml
-! -iname \*.xml
-! -iname \*.md
-! -iname .\*
-! -path ./_layouts/\*
-! -path ./.git/\*
-! -path ./css/\*
-! -path ./js/\*
-! -path ./img/\*
-! -path ./tmp/\*
-! -path ./fonts/\*
-! -path ./stats/\*
-)
+: ${f:=${*:-*}}${URL:="https://ten.j-j.net/"}
 
 cat <<HEADER
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 HEADER
 
-cd $cd
-
-find . -type f "${OPTIONS[@]}" -printf "%TY-%Tm-%Td%p\n" | \
-while read -r line; do
+find $f -mindepth 1 -not -iname '.git' -print | while read -r f; do
+l="$(git log --pretty="%ai $f" -1 HEAD -- $f)"
 cat << ITEM
 <url>
-	<loc>${URL}${line:12}</loc>
-	<lastmod>${line:0:10}</lastmod>
+	<loc>${URL}${l:26}</loc>
+	<lastmod>${l:0:10}</lastmod>
 	<changefreq>weekly</changefreq>
 	<priority>0.5</priority>
 </url>
